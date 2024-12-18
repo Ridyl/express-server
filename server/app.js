b9842dbaed0ee512ffcbf0f550412b48ad3987b1
Ridyl <joey.dalatri214@gmail.com>
2024-12-18 14:45:44 -0500

    Created simple Express server with Morgan middleware that properly routes certain request and will display json data with GET /data

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 // import files and packages up here
+const express = require('express');
+const data = require('./data.json');
+const morgan = require('morgan');
 
+// instantiate your express server below
+const app = express();
 
-// create your express server below
-var app;
+app.use(morgan('dev'));
 
 // add your routes and middleware below
+app.get('/', (req, res) => {
+    res.status(200).send('works');
+});
+
+app.get('/data', (req, res) => {
+    // res.sendStatus(200);
+    res.status(200).type('json').send(data);
+});
+
 
 // finally export the express application
 module.exports = app;
